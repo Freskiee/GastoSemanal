42bfb319b1252fc4e8094aef817ea88a0749e01f
Ariel Sanchez <freska_96@hotmail.com>
2024-11-13 11:45:40 -0600

    feat: Creando e instanciando la clase de Presupuesto

--- js/app.js
+++ js/app.js
@@ -7,19 +7,32 @@ const gastoListado = document.querySelector('#gastos ul');
 //! Eventos
 eventListeners();
 function eventListeners() {
-    document,addEventListener('DOMContentLoaded', preguntarPresupuesto);
+    document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
 };
 
 //! Clases
+class Presupuesto {
+    constructor(presupuesto) {
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gastos = [];
+    };
+};
 
+//! Instanciar
+class UI {};
+let presupuesto;
 
 //! Funciones
 function preguntarPresupuesto() {
-    const presupuestoUsuario = prompt('¿Cuál es tu presupuesto?')
-    console.log(Number(presupuestoUsuario));
+    const presupuestoUsuario = prompt('¿Cuál es tu presupuesto?');
     if(presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0) {
         window.location.reload();
     };
+
+    //~ Presupuesto valido
+    presupuesto = new Presupuesto(presupuestoUsuario);
+    console.log(presupuesto);
 };
 
 
