5a09ce5ce083344d1948fe507ea061c26f8eb06a
Ariel Sanchez <freska_96@hotmail.com>
2024-11-13 11:37:57 -0600

    feat: primeros pasos en el proyecto

--- js/app.js
+++ js/app.js
@@ -1,4 +1,27 @@
 
+//! Variables y Selectores
+const formulario = document.querySelector('#agregar-gasto');
+const gastoListado = document.querySelector('#gastos ul');
+
+
+//! Eventos
+eventListeners();
+function eventListeners() {
+    document,addEventListener('DOMContentLoaded', preguntarPresupuesto);
+};
+
+//! Clases
+
+
+//! Funciones
+function preguntarPresupuesto() {
+    const presupuestoUsuario = prompt('¿Cuál es tu presupuesto?')
+    console.log(Number(presupuestoUsuario));
+    if(presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0) {
+        window.location.reload();
+    };
+};
+
 
 
 
