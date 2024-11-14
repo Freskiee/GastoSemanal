dd9763102e2fa7476109b598c623722c4548f083
Ariel Sanchez <freska_96@hotmail.com>
2024-11-14 05:38:58 -0600

    feat: Actualizar el Presupuesto Restante

--- js/app.js
+++ js/app.js
@@ -21,11 +21,18 @@ class Presupuesto {
 
     nuevoGasto(gasto) {
         this.gastos = [...this.gastos, gasto];
-        console.log(this.gastos);
+        this.calcularRestante();
+    };
+
+    calcularRestante() {
+        const gastado = this.gastos.reduce((total, gasto) => total + gasto.cantidad, 0);
+        this.restante = this.presupuesto - gastado;
+        console.log(this.restante);
     };
 };
 
 class UI {
+
     insertarPresupuesto(cantidad) {
         //~ Extrayendo los valores
         const { presupuesto, restante } = cantidad;
@@ -43,13 +50,10 @@ class UI {
         } else {
             divMensaje.classList.add('alert-success');
         };
-
         //~ Mensaje de error
         divMensaje.textContent = mensaje;
-
         //~ Insertar en el HTML
         document.querySelector('.primario').insertBefore(divMensaje, formulario);
-
         //~ Quitar del HTML
         setTimeout(() => {
             divMensaje.remove();
@@ -57,39 +61,35 @@ class UI {
     };
 
     agregarGastoListado(gastos) {
-
         this.limpiarHTML(); // Elimina el HTML previo
-
         //~ Iterar sobre los gastos
         gastos.forEach(gasto => {
-
             const { cantidad, nombre, id } = gasto;
-
             //~ Crear un LI
             const nuevoGasto = document.createElement('LI');
             nuevoGasto.className = 'list-group-item d-flex justify-content-between align-items-center';
             // nuevoGasto.setAttribute('data-id', id); <-- Es lo mismo que el codigo de abajo, es otra manera de hacerlo
             nuevoGasto.dataset.id = id;
-
             //~ Agregar el HTML del gasto
-            nuevoGasto.innerHTML = `${nombre} <span class="badge badge-primary badge-pill">${cantidad}</span>`;
-
+            nuevoGasto.innerHTML = `${nombre} <span class="badge badge-primary badge-pill">$${cantidad}</span>`;
             //~ Boton para borrar el gasto
             const btnBorrar = document.createElement('BUTTON');
             btnBorrar.classList.add('btn', 'btn-danger', 'borrar-gasto');
             btnBorrar.innerHTML = 'Borrar &times'; // El &times es para agregar una 'X', solo lo detecta si lo usas con innerHTML, no con textContent
             nuevoGasto.appendChild(btnBorrar);
-
             //~ Agregar al HTML
             gastoListado.appendChild(nuevoGasto);
-
         });
     };
 
     limpiarHTML() {
-        while(gastoListado.firstChild) {
+        while (gastoListado.firstChild) {
             gastoListado.removeChild(gastoListado.firstChild);
         };
+    };
+
+    actualizarRestante(restante) {
+        document.querySelector('#restante').textContent = restante;
     };
 };
 
@@ -140,8 +140,9 @@ function agregarGasto(e) {
     ui.imprimirAlerta('Gasto agregado correctamente');
 
     //~ Imprimir los gastos
-    const { gastos } = presupuesto;
+    const { gastos, restante } = presupuesto;
     ui.agregarGastoListado(gastos);
+    ui.actualizarRestante(restante);
 
     //~ Reinicia el formulario
     formulario.reset();
