6dd1118fe635d46aa10a83714611cfbb199fa5ba
Ariel Sanchez <freska_96@hotmail.com>
2024-11-14 05:28:42 -0600

    feat: Mostrar los Gastos en el HTML

--- js/app.js
+++ js/app.js
@@ -55,6 +55,42 @@ class UI {
             divMensaje.remove();
         }, 3000);
     };
+
+    agregarGastoListado(gastos) {
+
+        this.limpiarHTML(); // Elimina el HTML previo
+
+        //~ Iterar sobre los gastos
+        gastos.forEach(gasto => {
+
+            const { cantidad, nombre, id } = gasto;
+
+            //~ Crear un LI
+            const nuevoGasto = document.createElement('LI');
+            nuevoGasto.className = 'list-group-item d-flex justify-content-between align-items-center';
+            // nuevoGasto.setAttribute('data-id', id); <-- Es lo mismo que el codigo de abajo, es otra manera de hacerlo
+            nuevoGasto.dataset.id = id;
+
+            //~ Agregar el HTML del gasto
+            nuevoGasto.innerHTML = `${nombre} <span class="badge badge-primary badge-pill">${cantidad}</span>`;
+
+            //~ Boton para borrar el gasto
+            const btnBorrar = document.createElement('BUTTON');
+            btnBorrar.classList.add('btn', 'btn-danger', 'borrar-gasto');
+            btnBorrar.innerHTML = 'Borrar &times'; // El &times es para agregar una 'X', solo lo detecta si lo usas con innerHTML, no con textContent
+            nuevoGasto.appendChild(btnBorrar);
+
+            //~ Agregar al HTML
+            gastoListado.appendChild(nuevoGasto);
+
+        });
+    };
+
+    limpiarHTML() {
+        while(gastoListado.firstChild) {
+            gastoListado.removeChild(gastoListado.firstChild);
+        };
+    };
 };
 
 //! Instanciar
@@ -62,6 +98,8 @@ const ui = new UI();
 let presupuesto;
 
 //! Funciones
+
+//* Extrae el presupuesto
 function preguntarPresupuesto() {
     const presupuestoUsuario = prompt('¿Cuál es tu presupuesto?');
     if (presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0) {
@@ -100,6 +138,10 @@ function agregarGasto(e) {
 
     //~ Mensaje de todo bien!!!
     ui.imprimirAlerta('Gasto agregado correctamente');
+
+    //~ Imprimir los gastos
+    const { gastos } = presupuesto;
+    ui.agregarGastoListado(gastos);
 
     //~ Reinicia el formulario
     formulario.reset();
